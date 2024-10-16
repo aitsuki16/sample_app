c2f64e6a309ea581a824c6eb5553a847d3efb7b9
aitsuki16 <aitsuki26@gmail.com>
2024-10-16 12:31:39 +0900

    added test-->

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  root "application#hello"
+  # Group routes related to static pages
+  get 'home', to: 'static_pages#home'
+  get 'help', to: 'static_pages#help'
+  get 'about',to: 'static_pages#about'
 
+  # Define root route
+  root 'static_pages#home'
 end
