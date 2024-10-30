bb401b245a05fa2496fb584f3d9c9f8cba6b3330
aitsuki16 <aitsuki26@gmail.com>
2024-10-30 22:05:54 +0900

    Added sign up form 7.2, 7.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,40 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
-
+    @user = User.new  # Create a new user instance for the signup form
+    Rails.logger.debug("Params: #{params.inspect}") 
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user  # Redirect to user's profile page on success
+    else
+      render 'new'  # Render signup form again on failure
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  # Root route (homepage)
   root "static_pages#home"
+
+  # Static pages routes
   get "/help", to: "static_pages#help", as: "help"
   get "/about", to: "static_pages#about", as: "about"
   get "/contact", to: "static_pages#contact", as: "contact"
 
-  resources :users
+  # Signup route mapped to users#new
+  get "/signup", to: "users#new", as: "signup"
 
+  # User resource routes (new, create, show)
+  resources :users, only: [:new, :create, :show]
 end
 
