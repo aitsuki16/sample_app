972510093424c44bdf593b6f36ba59322662b3f1
aitsuki16 <aitsuki26@gmail.com>
2024-11-01 11:22:46 +0900

    added log in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
+      redirect_to user
     else
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       # Create an error message.
-    render 'new', status: :unprocessable_entity
+      render 'new', status: :unprocessable_entity
+    end # This 'end' closes the 'if' statement
   end
 
   def destroy
   end
-
 end
