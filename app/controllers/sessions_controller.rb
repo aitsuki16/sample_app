d496f691493c5ad87f6a3d589159d7a403208298
aitsuki16 <aitsuki26@gmail.com>
2024-11-04 12:56:47 +0900

    9.1.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       reset_session
+      remember user
       log_in user
       redirect_to user
     else
