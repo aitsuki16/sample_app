b4f344b78eb5a5717cb48b47fe45bec9a05956dd
aitsuki16 <aitsuki26@gmail.com>
2024-11-08 10:28:32 +0900

    11.3.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,7 @@
 class User < ApplicationRecord
-  
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
-
-  # Accessible attribute for remember token
-  attr_accessor :remember_token
-
-  # Ensure email is saved as lowercase before saving
-  before_save { self.email = email.downcase }
 
   # Validation for name: required and with a max length of 50 characters
   validates :name, presence: true, length: { maximum: 50 }
@@ -25,6 +18,13 @@ class User < ApplicationRecord
   # Enable password hashing and authentication methods
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -44,19 +44,13 @@ class User < ApplicationRecord
     remember_digest
   end
 
-    # Returns a session token to prevent session hijacking.
+  # Returns a session token to prevent session hijacking.
   # We reuse the remember digest for convenience.
   def session_token
     remember_digest || remember
   end
 
-    # Returns true if the given token matches the digest.
-    def authenticated?(remember_token)
-      return false if remember_digest.nil?
-      BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end
-
-      # Forgets a user.
+  # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,7 +7,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 
   test "email addresses should be unique" do
@@ -33,5 +33,7 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+
 
 end
