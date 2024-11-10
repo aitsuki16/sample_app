e53b465edc76abe86909e9af45eabce29e78f60a
aitsuki16 <aitsuki26@gmail.com>
2024-11-10 11:11:37 +0900

    Prepare for redeployment on Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,6 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
-
-  # def index
-  #   # @users = User.all
-  #   @users = User.paginate(page: params[:page])
-
-  # end
-
   def index
     @users = User.where(activated: true).paginate(page: params[:page])
   end
@@ -18,9 +11,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated?
   end
-  # def show
-  #   @user = User.find(params[:id])
-  # end
 
   def new
     @user = User.new
@@ -36,18 +26,6 @@ class UsersController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
-
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     reset_session
-  #     log_in @user
-  #     flash[:success] = "Welcome to the Sample App!"
-  #     redirect_to @user
-  #   else
-  #     render 'new', status: :unprocessable_entity
-  #   end
-  # end
 
   def edit
     @user = User.find(params[:id])
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,38 +1,17 @@
 require "test_helper"
 
-# class UserMailerTest < ActionMailer::TestCase
-#   test "account_activation" do
-#     mail = UserMailer.account_activation
-#     assert_equal "Account activation", mail.subject
-#     assert_equal ["to@example.org"], mail.to
-#     assert_equal ["from@example.com"], mail.from
-#     assert_match "Hi", mail.body.encoded
-#   end
-
-#   test "password_reset" do
-#     mail = UserMailer.password_reset
-#     assert_equal "Password reset", mail.subject
-#     assert_equal ["to@example.org"], mail.to
-#     assert_equal ["from@example.com"], mail.from
-#     assert_match "Hi", mail.body.encoded
-#   end
-
-# end
-
-
-require "test_helper"
-
 class UserMailerTest < ActionMailer::TestCase
-
   test "account_activation" do
-    user = users(:michael)
-    user.activation_token = User.new_token
-    mail = UserMailer.account_activation(user)
+    user = users(:michael) # Use a test user fixture (replace 'michael' with your actual fixture)
+    user.activation_token = User.new_token # Assign a new activation token
+    mail = UserMailer.account_activation(user) # Create the email
+
+    # Test email details
     assert_equal "Account activation", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["aidaiga16@gmail.com"], mail.from
-    assert_match user.name,               mail.body.encoded
-    assert_match user.activation_token,   mail.body.encoded
-    assert_match CGI.escape(user.email),  mail.body.encoded
+    assert_equal ["igaaida16@gmail.com"], mail.from # Update to your actual 'from' address 
+    assert_match user.name, mail.body.encoded
+    assert_match user.activation_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
   end
 end
