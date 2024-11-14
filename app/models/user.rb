176751920389ed365051ef49b70720346a83a911
aitsuki16 <aitsuki26@gmail.com>
2024-11-14 14:33:59 +0900

    fixed "end" error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -23,7 +22,6 @@ class User < ApplicationRecord
   # Activates an account.
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
-
   end
 
   # Sends activation email.
@@ -39,13 +37,13 @@ class User < ApplicationRecord
   end
 
   # Returns the hash digest of the given string.
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token.
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
@@ -70,20 +68,19 @@ class User < ApplicationRecord
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_digest, User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
 
   # Sends password reset email.
-  # def send_password_reset_email
-  #   reset_sent_at < 2.hours.ago
- # UserMailer.password_reset(self).deliver_now
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
- # Returns true if a password reset has expired.
- def password_reset_expired?
-  reset_sent_at < 2.hours.ago
 
-end
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
   private
 
