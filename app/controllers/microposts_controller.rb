d186c63f0443ed3dcc99d6440c844eb83862b3ea
aitsuki16 <aitsuki26@gmail.com>
2024-11-15 15:44:52 +0900

    added turbo

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,30 @@
+# class MicropostsController < ApplicationController
+#     before_action :logged_in_user, only: [:create, :destroy]
+  
+#     def create
+#       @micropost = current_user.microposts.build(micropost_params)
+#       if @micropost.save
+#         flash[:success] = "Micropost created!"
+#         redirect_to root_url
+#       else
+#         render 'static_pages/home', status: :unprocessable_entity
+#       end
+#     end
+  
+#     def destroy
+#     end
+  
+#     private
+  
+#       def micropost_params
+#         params.require(:micropost).permit(:content)
+#       end
+#   end
+  
+
 class MicropostsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
+    before_action :correct_user, only: :destroy
   
     def create
       @micropost = current_user.microposts.build(micropost_params)
@@ -7,11 +32,18 @@ class MicropostsController < ApplicationController
         flash[:success] = "Micropost created!"
         redirect_to root_url
       else
+        @feed_items = current_user.feed.paginate(page: params[:page]) # Ensure feed items are loaded
         render 'static_pages/home', status: :unprocessable_entity
       end
     end
   
     def destroy
+      @micropost = Micropost.find(params[:id])
+      @micropost.destroy
+      respond_to do |format|
+        format.html { redirect_to root_url, status: :see_other }
+        format.turbo_stream
+      end
     end
   
     private
@@ -19,6 +51,10 @@ class MicropostsController < ApplicationController
       def micropost_params
         params.require(:micropost).permit(:content)
       end
-  end
   
+      def correct_user
+        @micropost = current_user.microposts.find_by(id: params[:id])
+        redirect_to root_url, status: :see_other if @micropost.nil?
+      end
+  end
   
