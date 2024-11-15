6e18f2bfb7c1717be4e2338fafebf810aa653645
aitsuki16 <aitsuki26@gmail.com>
2024-11-15 14:16:40 +0900

    13.3.2 Creating microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,21 +61,49 @@ class UsersController < ApplicationController
         # Before filters
 
     # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
-        # Confirms the correct user.
-     def correct_user
-       @user = User.find(params[:id])
-       redirect_to(root_url, status: :see_other) unless current_user?(@user)
-      end
 
-          # Confirms an admin user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
     def admin_user
       redirect_to(root_url, status: :see_other) unless current_user.admin?
     end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     def logged_in_user
+#       unless logged_in?
+#         store_location
+#         flash[:danger] = "Please log in."
+#         redirect_to login_url, status: :see_other
+#       end
+#     end
+#         # Confirms the correct user.
+#      def correct_user
+#        @user = User.find(params[:id])
+#        redirect_to(root_url, status: :see_other) unless current_user?(@user)
+#       end
+
+#           # Confirms an admin user.
+#     def admin_user
+#       redirect_to(root_url, status: :see_other) unless current_user.admin?
+#     end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,41 @@
-Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  get 'sessions/new'
+# Rails.application.routes.draw do
+#   get 'password_resets/new'
+#   get 'password_resets/edit'
+#   get 'sessions/new'
   
-  root "static_pages#home"
+#   root "static_pages#home"
 
-  # Static pages routes
-  get "/help", to: "static_pages#help", as: "help"
-  get "/about", to: "static_pages#about", as: "about"
-  get "/contact", to: "static_pages#contact", as: "contact"
+#   # Static pages routes
+#   get "/help", to: "static_pages#help", as: "help"
+#   get "/about", to: "static_pages#about", as: "about"
+#   get "/contact", to: "static_pages#contact", as: "contact"
 
-  # Signup, login, and logout routes
-  get "/signup", to: "users#new", as: "signup"
-  get "/login",   to: "sessions#new"
-  post "/login",   to: "sessions#create"
+#   # Signup, login, and logout routes
+#   get "/signup", to: "users#new", as: "signup"
+#   get "/login",   to: "sessions#new"
+#   post "/login",   to: "sessions#create"
+#   delete "/logout",  to: "sessions#destroy"
+
+#   # User resource routes including the index action
+#   resources :users, only: [:index, :new, :create, :show, :edit, :update]
+
+#   resources :account_activations, only: [:edit]
+#   resources :password_resets,     only: [:new, :create, :edit, :update]
+# end
+
+
+
+Rails.application.routes.draw do
+  root   "static_pages#home"
+  get    "/help",    to: "static_pages#help"
+  get    "/about",   to: "static_pages#about"
+  get    "/contact", to: "static_pages#contact"
+  get    "/signup",  to: "users#new"
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
-
-  # User resource routes including the index action
-  resources :users, only: [:index, :new, :create, :show, :edit, :update]
-
+  resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 end
