b5946fef2dc90afcf6cbfe006778e85dcb578734
aitsuki16 <aitsuki26@gmail.com>
2024-11-18 11:36:46 +0900

    added for follow and unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,3 @@
-# Rails.application.routes.draw do
-#   get 'password_resets/new'
-#   get 'password_resets/edit'
-#   get 'sessions/new'
-  
-#   root "static_pages#home"
-
-#   # Static pages routes
-#   get "/help", to: "static_pages#help", as: "help"
-#   get "/about", to: "static_pages#about", as: "about"
-#   get "/contact", to: "static_pages#contact", as: "contact"
-
-#   # Signup, login, and logout routes
-#   get "/signup", to: "users#new", as: "signup"
-#   get "/login",   to: "sessions#new"
-#   post "/login",   to: "sessions#create"
-#   delete "/logout",  to: "sessions#destroy"
-
-#   # User resource routes including the index action
-#   resources :users, only: [:index, :new, :create, :show, :edit, :update]
-
-#   resources :account_activations, only: [:edit]
-#   resources :password_resets,     only: [:new, :create, :edit, :update]
-# end
-
-
-
 Rails.application.routes.draw do
   root   "static_pages#home"
   get    "/help",    to: "static_pages#help"
@@ -34,8 +7,15 @@ Rails.application.routes.draw do
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+
+  get '/microposts', to: 'static_pages#home'
 end
