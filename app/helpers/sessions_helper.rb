ae0d3a15b9c6f8cc12385dd033ee2e29837f66bf
aitsuki16 <aitsuki26@gmail.com>
2024-11-21 13:55:13 +0900

    user.delete.all

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,11 +17,13 @@ module SessionsHelper
     def current_user
       if (user_id = session[:user_id])
         user = User.find_by(id: user_id)
+        Rails.logger.debug "Current user from session: #{user.inspect}"
         @current_user ||= user if session[:session_token] == user.session_token
       elsif (user_id = cookies.encrypted[:user_id])
         user = User.find_by(id: user_id)
         if user && user.authenticated?(:remember, cookies[:remember_token])
           log_in user
+          Rails.logger.debug "Current user from cookies: #{user.inspect}"
           @current_user = user
         end
       end
